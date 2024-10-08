4698eed6e8824e618c5adf8855a99b10cd459d7c
Sam Farnsley <samfarnsley@gmail.com>
2024-10-08 13:14:03 -0700

    Ready up system (Start game is really late and laggy) Next fix will be that

---
+++ Assets/ReadyUp.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+
+public class ReadyUp : MonoBehaviour
+{
+    int numPlayersReady = 0;
+    public PhotonView view;
+    public Button readyButton;
+    public TextMeshProUGUI text;
+    public SceneLoader sceneLoader;
+
+    public void OnClick()
+    {
+        view.RPC("IncrementReadyPlayers", RpcTarget.AllBuffered);
+        readyButton.gameObject.SetActive(false);
+    }
+
+    [PunRPC]
+    private void IncrementReadyPlayers()
+    {
+        numPlayersReady++;
+        text.text = numPlayersReady + "/2";
+
+        // Check the number of players ready after the RPC update
+        CheckNumPlayers();
+    }
+
+    private void CheckNumPlayers()
+    {
+        if (numPlayersReady == PhotonNetwork.CurrentRoom.PlayerCount)
+        {
+            sceneLoader.LoadScene(4);
+        }
+    }
+}
